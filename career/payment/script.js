567a7c121d3e9e1849892ce1396c98450ce828d4
wasiu.abiola <wasiu.abiola@swapmoney.onmicrosoft.com>
2024-11-16 15:29:03 -0800

    updated

--- career/payment/script.js
+++
@@ -1,9 +0,0 @@
-function proceed(){
-    setTimeout(() =>{
-        document.getElementById("payment_form").submit;
-        alert("Your payment information has been successfully submitted for review");
-    }, 1000);
-
-    var addressDomain = window.location.origin;
-    document.getElementById("_nextPage").value = addressDomain + "/career/payment/index.html";
-} 
